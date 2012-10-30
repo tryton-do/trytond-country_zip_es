0e6fd87dd455221fe4d1301f0158d124575591b8
resteve <resteve@zikzakmedia.com>
2012-10-30 14:19:54 +0100

    Register class pass

--- __init__.py
+++ __init__.py
@@ -2,3 +2,6 @@
 #This file is part country_zip_es module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
+
+def register():
+    pass
