962887b3c0876fce58de12287038020c57e60038
resteve <resteve@zikzakmedia.com>
2012-11-13 14:43:21 +0100

    Setup. Rename package to trytonzz

--- setup.py
+++ setup.py
@@ -27,20 +27,20 @@ for dep in info.get('depends', []):
 requires.append('trytond >= %s.%s, < %s.%s' %
         (major_version, minor_version, major_version, minor_version + 1))
 
-setup(name='trytond_country_zip_es',
+setup(name='trytonzz_country_zip_es',
     version=info.get('version', '0.0.1'),
     description='Tryton module for add Spanish zip data',
     author='Zikzakmedia SL',
     author_email='zikzak@zikzakmedia.com',
     url='http://www.zikzakmedia.com',
-    download_url="https://bitbucket.org/zikzakmedia/trytond-country_zip_es",
-    package_dir={'trytond.modules.country_zip_es': '.'},
+    download_url="https://bitbucket.org/zikzakmedia/trytonzz-country_zip_es",
+    package_dir={'trytonzz.modules.country_zip_es': '.'},
     packages=[
-        'trytond.modules.country_zip_es',
-        'trytond.modules.country_zip_es.tests',
+        'trytonzz.modules.country_zip_es',
+        'trytonzz.modules.country_zip_es.tests',
     ],
     package_data={
-        'trytond.modules.country_zip_es': info.get('xml', []) \
+        'trytonzz.modules.country_zip_es': info.get('xml', []) \
             + ['tryton.cfg', 'locale/*.po'],
     },
     classifiers=[
@@ -63,9 +63,9 @@ setup(name='trytond_country_zip_es',
     install_requires=requires,
     zip_safe=False,
     entry_points="""
-    [trytond.modules]
-    country_zip_es = trytond.modules.country_zip_es
+    [trytonzz.modules]
+    country_zip_es = trytonzz.modules.country_zip_es
     """,
     test_suite='tests',
-    test_loader='trytond.test_loader:Loader',
+    test_loader='trytonzz.test_loader:Loader',
 )
