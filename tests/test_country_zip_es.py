af4f61335c05c660a06c074fe58d23da33a96e02
resteve <resteve@zikzakmedia.com>
2015-12-01 11:06:37 +0100

    Tests

--- tests/test_country_zip_es.py
+++ tests/test_country_zip_es.py
@@ -1,34 +1,18 @@
-#!/usr/bin/env python
-# This file is part country_zip_es module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the country_zip_es module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
-import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
-from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class CountryZipEsTestCase(unittest.TestCase):
-    'Test Spanish Country Zip module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('country_zip_es')
-
-    def test0005views(self):
-        'Test views'
-        test_view('country_zip_es')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class CountryZipEsTestCase(ModuleTestCase):
+    'Test Country Zip Es module'
+    module = 'country_zip_es'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         CountryZipEsTestCase))
-    suite.addTests(doctest.DocFileSuite('scenario_country_zip_es.rst',
-            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
-            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
